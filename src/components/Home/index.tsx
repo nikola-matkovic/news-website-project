97c08b9680a3397e940f0e50864723b113dcbd56
anonymousvegan <majjunjulavgust@gmail.com>
2022-09-04 22:59:21 +0200

    feat: Add router page

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../Layout";
+
+const Home = () => {
+    return (
+        <Layout>
+            Home
+        </Layout> 
+    );
+}
+
+export default Home;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,14 @@
+import Nav from "../Nav";
+import Footer from "../Footer";
+
+const Layout = ( props: any ) => {
+    return (
+        <>
+            <Nav/>
+                {props.children}
+            <Footer/>
+        </> 
+    );
+}
+
+export default Layout;
