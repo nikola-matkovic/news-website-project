e2ec27b80f6daa9f9acc7600dfa671bee55019f0
anonymousvegan <majjunjulavgust@gmail.com>
2022-09-06 13:44:39 +0200

    fetch news

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,29 @@
 import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import Layout from "../Layout";
+import key  from "./../api/key.json";
 
 const Home = () => {
     const [news, setNews] = useState([]);
+
+    useEffect( ()=> {
+        axios.get('https://newsapi.org/v2/top-headlines',{
+            params:{
+                q:"name",
+                sortBy:"filter",
+                pageSize:"page",
+                from:"fromDate",
+                to:"toDate",
+                apiKey:key.key
+            }
+           }).then((response)=>{
+            setNews(response.data.articles)
+           })
+    }, [])
+
     return (
         <Layout>
-            
+            {console.log(news)}
         </Layout> 
     );
 }
