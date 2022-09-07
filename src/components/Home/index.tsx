6bbee69e0c79e4319446396272183809f402cf8e
anonymousvegan <majjunjulavgust@gmail.com>
2022-09-07 18:34:32 +0200

    Add alll posible filters

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,29 +1,50 @@
 import axios from "axios";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, /*useContext*/ } from "react";
 import Layout from "../Layout";
 import key  from "./../api/key.json";
+import style from "./style.module.css"
 
 const Home = () => {
+    const apiKey = key.key;
     const [news, setNews] = useState([]);
-
+    const [country, setCountry] = useState("")
+    const [category, setCategory] = useState("technology");
+    const [q, setQ] = useState("");
+    const [pageSize, setPageSize] = useState(20);
     useEffect( ()=> {
-        axios.get('https://newsapi.org/v2/top-headlines',{
+        if(country === ""){
+            axios.get('https://ipapi.co/json/')
+            .then((response) => {
+                let data = response.data;
+                setCountry(data.country_code);
+            })
+        }
+        axios.get('https://newsapi.org/v2/top-headlines', {
             params:{
-                q:"name",
-                sortBy:"filter",
-                pageSize:"page",
-                from:"fromDate",
-                to:"toDate",
-                apiKey:key.key
+                apiKey,
+                q,
+                pageSize,
+                country,
+                category,
             }
-           }).then((response)=>{
+        })
+        .then((response)=>{
             setNews(response.data.articles)
-           })
-    }, [])
+            console.log(news);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }, [country]);
+
+    let article = news[0];
+    console.log(article);
 
     return (
-        <Layout>
-            {console.log(news)}
+        <Layout> 
+            <section className={style.articleCard}>
+                
+            </section>
         </Layout> 
     );
 }
