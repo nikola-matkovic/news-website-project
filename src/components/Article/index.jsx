b14f9be1c20caf9c11d95994361e830c56506c08
anonymousvegan <majjunjulavgust@gmail.com>
2022-09-13 12:49:20 +0200

    Finish article page

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -1,12 +1,25 @@
 import Layout from "../Layout";
 import {useLocation} from 'react-router-dom';
+import style from './style.module.css';
 
 const Articel = () => {
     const location = useLocation();
-    console.log(location);
+    let article = location.state.article;
+    let {author, content, description, publishedAt, source, title, url, urlToImage} = article;
+    
+    publishedAt = publishedAt.substring( 0, publishedAt.indexOf('T'))
+
+    console.log(author, content, description, publishedAt, source, title, url, urlToImage);
     return (
         <Layout> 
-            <div>test</div>
+            <article className={style.article}>
+                <h2>{title}</h2>
+                <img src={urlToImage} alt="articleImage" />
+                <p>{content}</p>
+                <a href={url}> Pročitaj više</a>
+
+                <div className={style.info}> <span>{author} </span>  <span>{publishedAt} </span>  <span> {source.name} </span> </div>
+            </article>
         </Layout>
     )
 }
